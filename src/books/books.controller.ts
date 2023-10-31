7d096ed8d2a96083589882944517ef680ef5247f
Nikita Sokolov <mr.sokolov99@gmail.com>
2023-10-31 19:48:30 +0300

    added validation

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -5,10 +5,13 @@ import {
   Get,
   Param,
   Post,
-  Put
+  Put,
+  UsePipes
 } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto, UpdateBookDto } from './interfaces/books';
+import { JoiValidationPipe } from 'src/validation/pipes/validation.pipe';
+import { bookCreateSchema, bookUpdateSchema } from './schemas/book.schema';
 
 @Controller('books')
 export class BooksController {
@@ -24,13 +27,19 @@ export class BooksController {
     return this.service.getBook(id);
   }
 
+  @UsePipes(new JoiValidationPipe(bookCreateSchema))
   @Post()
   create(@Body() book: CreateBookDto) {
     return this.service.createBook(book);
   }
 
+  @UsePipes(new JoiValidationPipe(bookUpdateSchema))
   @Put(':id')
-  update(@Param('id') id: Id, @Body() book: UpdateBookDto) {
+  update(
+    @Param('id') id: Id,
+    @Body()
+    book: UpdateBookDto
+  ) {
     return this.service.updateBook(id, book);
   }
 
